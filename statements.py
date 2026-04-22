0c53df1617037d70863d552f8646e62fb18c532d
David <wafuladavid1987@gmail.com>
2026-04-22 16:36:50 +0300

    conditional statements

---
+++ statements.py
@@ -0,0 +1,40 @@
+if 20>5:
+    print("20 is greater")
+else:
+    print("20 is less")
+
+#check if someone is eligible to vote
+age=17
+if age>=18:
+    print("eligible to vote")
+else:
+    print("not eligible to vote")
+
+#temperatue
+temperature=30
+if temperature>=30:
+    print ("too hot")
+else:
+    print("normal temperature")   
+
+
+marks=88
+if marks>=80:
+    print("grade A")
+elif marks>=70 and marks<80:
+    print("grade b")
+elif marks>=60 and marks<70:
+    print("grade c")
+elif marks>=50 and marks<60:
+    print("grade d")
+else:
+    print("grade E")
+
+#TEMPERATURE
+temperature=27
+if temperature>=30:
+    print("too hot")
+elif temperature>15 and temperature<30:
+    print("normal temperature")
+else:
+    print("normal temperature")
